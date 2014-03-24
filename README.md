c49135d57c79008a3a566fc9c34b0644e524a6e0
Patrick Stein <Patrick.Stein@krokodil.jinx.de>
2014-03-24 10:18:25 +0100

    first fritzbox presence script

---
+++ README.md
@@ -0,0 +1,12 @@
+Homematic Scripts
+-----------------
+
+The following are scripts I use in conjunction with my Homematic installation. I do tend not to use additional modules / software on my systems, so things should work without additions.
+
+
+fritzbox presence script
+------------------------
+
+The script set or unsets the variables on a CCU2 when a users presence is detected on the Fritz box. You can add devices by their IP Address or Ethernet address or even IP ranges for detection of guests for example.
+
+
---
+++ fritzboxpresence.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+# presence detection for home router (e.g. Fritzbox)
+# sets variables on homematics ccu2 in this incarnation (change the wget)
+# can be found on: https://github.com/jollyjinx/homematic
+#
+#	install this script on a fritzbox and start it after reboot via debug.cfg
+#
+
+hmccu2="hmccu2"					# ip address of ccu2
+set patrick guests				# variables to set on the ccu2 and their presence check
+patrick="a:f5:90:44:24:a4"
+guests="5:77:4f:e8:87:77|89:0b:91:ea:d2:33|d0:8d:a6:d7:f1:5a|c9:57:54:b8:ae:c1|192.168.0.1[5-8][0-9]"
+
+looptime=30	# how often we check in seconds
+presence=20	# how many loops until somebody is no longer present
+
+
+for name in $@
+do
+	eval old$name=`expr $presence / 2 + 1`
+done
+
+while true
+do
+        arpoutput=$(arp -an |grep at)
+
+        for name in $@
+        do
+                eval addresstocheck=\$$name
+                grepoutput=$(echo $arpoutput|egrep -i "$addresstocheck")
+
+               	eval oldvalue=\$old$name
+
+               	if [ -n "$grepoutput" ];
+                then
+                		eval old$name=$presence
+                        isalive=1
+                else
+                		eval old$name=`expr $oldvalue - 1 \| 1 `
+                        isalive=0
+                fi
+
+               	eval newvalue=\$old$name
+
+                if [ $newvalue != $oldvalue -a \( $newvalue = 1 -o $newvalue = $presence \) ] ;
+                then
+                	#echo "Changing state: $name $isalive $newvalue"
+					wget -q -O /dev/null "http://$hmccu2:8181/rega.exe?state=dom.GetObject('$name').State($isalive)"
+                fi
+        done
+
+        sleep $looptime
+done
+
